f1258164238c3a3f636a144023062d7b32407024
Dan <dan@DandeMacBook-Pro.local>
2023-05-25 10:56:41 +0800

    feat: :sparkles: message init

---
+++ apps/web/src/app/Messsage/locales/zh-cn.ts
@@ -0,0 +1,16 @@
+const zh: {
+  [k: string]: string;
+} = {
+  'message.center': '消息中心',
+  'r.all': '全部',
+  'r.readed': '已读',
+  'r.unread': '未读',
+  'r.time': '时间',
+  'r.search': '搜索',
+  'go.demand.list': '进入订单列表',
+  'view.detail': '查看详情',
+  'go.data.detail': '进入投后数据看板',
+  'go.person.center': '进入个人中心',
+};
+
+export default zh;
