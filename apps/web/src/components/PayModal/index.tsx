7093d24ca678ebfeb15a63ef4c7f30bb8ce17075
Dan <dan@192.168.110.14>
2023-06-07 10:10:46 +0800

    feat: :sparkles: git push
    delete 0

--- apps/web/src/components/PayModal/index.tsx
+++ apps/web/src/components/PayModal/index.tsx
@@ -88,7 +88,7 @@ const index: React.FC<PayModalProps> = ({ open, handleCloseModal, activityId }) 
         {data?.available_balance === 0 && (
           <div className={classNames(styles['orange'], styles['insuffient-tip'])}>{i18n[lang]['go-person-center']}</div>
         )}
-        {data?.available_balance && data?.available_balance < passMount && (
+        {!!data?.available_balance && data?.available_balance < passMount && (
           <div className={classNames(styles['orange'], styles['insuffient-tip'])}>{i18n[lang]['go-person-center']}</div>
         )}
       </div>
