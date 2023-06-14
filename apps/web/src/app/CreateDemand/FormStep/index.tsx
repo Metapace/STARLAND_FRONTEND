24f7dc34d2e330c2f3eb7832611fc7dbfc36b898
Dan <dan@DandeMBP.lan>
2023-06-14 14:24:10 +0800

    build: :package: 22

--- apps/web/src/app/CreateDemand/FormStep/index.tsx
+++ apps/web/src/app/CreateDemand/FormStep/index.tsx
@@ -235,7 +235,7 @@ const Index: React.FC<FormStepProps> = ({
                   rules={[{ required: true, message: `${i18n[lang]['promte.budget']} ${i18n[lang]['is.required']}` }]}
                   field="price"
                   requiredSymbol={{ position: 'end' }}
-                  defaultValue={102}
+                  defaultValue={100}
                 >
                   <InputNumber
                     min={100}
