84fc116b1dd6a2244f64a53e9b2faba3540af558
Dan <dan@192.168.110.42>
2023-06-29 17:00:01 +0800

    feat: :sparkles: d

--- apps/admin/src/app/Login/index.tsx
+++ apps/admin/src/app/Login/index.tsx
@@ -44,6 +44,7 @@ export const Login: React.FC = () => {
   });
 
   const onSubmit = () => {
+    console.log('login', process.env.app_name === 'admin', process.env);
     form.validate(async (err, values) => {
       if (err) {
         return;
